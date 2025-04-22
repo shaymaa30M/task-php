359d7df88e4fad6a8215d9c8f526cb68078a2e9a
hospitals <shimshimo537@gmail.com>
2025-04-22 18:21:29 +0300

    first real commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="output.php" method="post">
+        <label for="name">name</label>
+        <input type="text" id="name" name="name" required><br>
+
+        <label for="email">email </label>
+        <input type="email" id="email" name="email" required><br>
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>
